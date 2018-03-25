fd885688381f3b395621c96bbb8748c06f2b8d01
Adrian Pankiewicz <farami11@tlen.pl>
2018-03-25 15:48:47 +0200

    Fix configuration

--- src/main/java/edu/bsk/confguration/AuthenticationConfiguration.java
+++ src/main/java/edu/bsk/confguration/AuthenticationConfiguration.java
@@ -1,44 +1,43 @@
-//package edu.bsk.confguration;
-//
-//import java.util.Collection;
-//import java.util.LinkedList;
-//
-//import javax.transaction.Transactional;
-//
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.security.core.GrantedAuthority;
-//import org.springframework.security.core.authority.SimpleGrantedAuthority;
-//import org.springframework.security.core.userdetails.UserDetails;
-//import org.springframework.security.core.userdetails.UserDetailsService;
-//import org.springframework.security.core.userdetails.UsernameNotFoundException;
-//
-//@Configuration
-//@CustomAuthentication
-//public class AuthenticationConfiguration implements UserDetailsService
-//{
-//	//@Autowired
-//	//private UserRepository userService;
-//
-//	@Override
-//	@Transactional
-//	public UserDetails loadUserByUsername(String nickname) throws UsernameNotFoundException
-//	{
-//		User user = userService.findByNickname(nickname);
-//		if(user == null)
-//			throw new UsernameNotFoundException(nickname);
-//		Collection<GrantedAuthority> grantedAuthorities = getGrantedAuthorities(user);
-//
-//		return new org.springframework.security.core.userdetails.User(user.getNickname(), user.getPassword(),
-//				grantedAuthorities);
-//	}
-//
-//	private Collection<GrantedAuthority> getGrantedAuthorities(User user)
-//	{
-//		Collection<GrantedAuthority> grantedAuthorities = new LinkedList<>();
-//		for (Role role : user.getRoles())
-//			grantedAuthorities.add(new SimpleGrantedAuthority("ROLE_" + role.getName()));
-//
-//		return grantedAuthorities;
-//	}
-//}
+package edu.bsk.confguration;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.LinkedList;
+
+import javax.transaction.Transactional;
+
+import edu.bsk.database.entities.User;
+import edu.bsk.database.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
+
+@Component
+@CustomAuthentication
+public class AuthenticationConfiguration implements UserDetailsService
+{
+	@Autowired
+	private UserRepository userRepository;
+
+	@Override
+	@Transactional
+	public UserDetails loadUserByUsername(String nickname) throws UsernameNotFoundException
+	{
+		User user = userRepository.findByNickname(nickname);
+		if(user == null)
+			throw new UsernameNotFoundException(nickname);
+		Collection<GrantedAuthority> grantedAuthorities = getGrantedAuthorities(user);
+
+		return new org.springframework.security.core.userdetails.User(user.getNickname(), user.getPassword(),
+				grantedAuthorities);
+	}
+
+	private Collection<GrantedAuthority> getGrantedAuthorities(User user)
+	{
+		return Collections.singleton(new SimpleGrantedAuthority("ROLE_" + user.getRole()));
+	}
+}
--- src/main/java/edu/bsk/confguration/SecurityConfiguration.java
+++ src/main/java/edu/bsk/confguration/SecurityConfiguration.java
@@ -13,18 +13,17 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
-@EnableGlobalMethodSecurity(securedEnabled = true)
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter
 {
-//	@Autowired
-//	@CustomAuthentication
-//	private UserDetailsService userDetailsService;
-//
-//	@Autowired
-//	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception
-//	{
-//		auth.userDetailsService(userDetailsService);
-//	}
+	@Autowired
+	@CustomAuthentication
+	private UserDetailsService userDetailsService;
+
+	@Autowired
+	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception
+	{
+		auth.userDetailsService(userDetailsService);
+	}
 	
 	@Bean
 	public PasswordEncoder passwordEncoder()
@@ -37,23 +36,22 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter
 	{ 
         http
             .authorizeRequests()
-                .antMatchers("/", "/authentication/**", "/register/**", "/register", "/javax.faces.resource/**", "/css/**",
-						"/images/**", "/js/**", "/error/**", "/debug/**", "/views/register/index.xhtml").permitAll()
-                .anyRequest().authenticated()
-//            .and()
-//            	.formLogin().loginPage("/authentication/login")
-//            	.usernameParameter("j_username")
-//            	.passwordParameter("j_password")
-//            	.failureUrl("/authentication/login?error=true")
-//                .loginProcessingUrl("/j_spring_security_check")
-//                .defaultSuccessUrl("/", true)
+				.antMatchers("/**").permitAll()
+                .anyRequest().permitAll()
+            .and()
+            	.formLogin().loginPage("/authentication/login")
+            	.usernameParameter("j_username")
+            	.passwordParameter("j_password")
+            	.failureUrl("/authentication/login?error=true")
+                .loginProcessingUrl("/j_spring_security_check")
+                .defaultSuccessUrl("/", true)
             .and()
                 .logout().logoutSuccessUrl("/")
 				.clearAuthentication(true)
 				.invalidateHttpSession(true)
             .and()
-            	.csrf().disable().csrf()
-            //.exceptionHandling().accessDeniedPage("/denied/index")
+            	.csrf().disable()
+            .exceptionHandling().accessDeniedPage("/denied/index")
             .and()
             	.sessionManagement()
 				.maximumSessions(1)
